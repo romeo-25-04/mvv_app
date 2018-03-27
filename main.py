bca324c84b1d975d84cf2ec0054a317886af44ec
lager6 <lager6@gmail.de>
2018-03-27 17:32:25 +0200

    requests package test

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import requests
+
+
 def main():
-    print("Hallo MVV")
+    payload = {'haltestelle': 'Hauptbahnhof',
+               'ubahn': '',
+               'sbahn': '',
+               'bus': '',
+               'tram': ''}
+    r = requests.get('https://www.mvg-live.de/ims/dfiStaticAnzeige.svc', params=payload)
+    print("Status code:\t", r.status_code)
+    print("request at:\t", r.url)
+    print("content-type:\t", r.headers['content-type'])
+    print("encoding:\t", r.encoding)
+    print('text:\t', r.text)
 
 
 if __name__ == '__main__':
